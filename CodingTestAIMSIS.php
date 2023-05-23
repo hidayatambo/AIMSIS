905bcad2efc372682333bb45c4b79857c3bbe663
dayat ambo <7420388-hidayatsyahidin@users.noreply.gitlab.com>
2023-05-23 17:59:35 +0700

    second commit

--- CodingTestAIMSIS.php
+++ CodingTestAIMSIS.php
@@ -24,7 +24,8 @@ function countWaysToMakeG3_5($amount, $coins)
 
 $coins = [200, 100, 50, 25, 10, 5, 1];
 $amount = 350; // G3.5
-How many different ways can G3.5 be made using any number of coins?”
 $ways = countWaysToMakeG3_5($amount, $coins);
 echo "Number of different ways to make G3.5: " . $ways;
-
+?>
+</body>
+</html>
